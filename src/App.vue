76e599d1b8785a19de7b91a07dba6f923c802c30
JKDow <jk.dowling22@gmail.com>
2024-12-23 11:23:53 +1100

    Pull out default code

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <!-- Navigation -->
+    <header class="hidden">
+        <div class="w-full bg-red-200">
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+            </nav>
+        </div>
+    </header>
+
+    <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <main>
+        <h1>Home 2</h1>
+    </main>
+</template>
