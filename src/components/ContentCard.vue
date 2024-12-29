bc32a62e8b73dbd4026a8e69c1d1614264e3b373
Josh Dowling <jk.dowling22@gmail.com>
2024-12-29 22:13:32 +1100

    Setup logos

--- src/components/ContentCard.vue
+++ src/components/ContentCard.vue
@@ -6,15 +6,15 @@ import { ref, defineProps } from 'vue'
 <template>
     <div class="w-full h-full grid grid-cols-5 grid-rows-3 gap-4">
         <ButtonLink color="blue" name="Github" href="https://github.com" />
-        <ButtonLink color="yellow" name="Gmail" href="https://mail.google.com" />
-        <ButtonLink color="pink" />
+        <ButtonLink color="yellow" />
+        <ButtonLink color="pink" name="Gmail" href="https://mail.google.com" />
         <ButtonLink color="purple" />
-        <ButtonLink color="green" />
+        <ButtonLink color="green" name="ChatGPT" href="https://chatgpt.com" />
         <ButtonLink color="pink" />
         <div class="w-full h-full col-span-3">
             <ButtonLink color="white" />
         </div>
-        <ButtonLink color="red" />
+        <ButtonLink color="red" name="Rust" href="https://www.rust-lang.org/learn" />
         <ButtonLink color="purple" />
         <ButtonLink color="blue" />
         <ButtonLink color="red" />
